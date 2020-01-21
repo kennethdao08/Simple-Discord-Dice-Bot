197032498ef653031035c5b42caa49d8dd6920ed
Kenneth Dao <kennethdao08@gmail.com>
2020-01-21 14:26:03 -0800

    initial commit

---
+++ dice.py
@@ -0,0 +1,19 @@
+import discord
+from random import randint
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "!")
+
+@bot.event
+async def on_ready():
+    print("Bot is online.")
+
+@bot.command()
+async def roll(ctx, num_of_rolls: int, sides: int):
+    #await ctx.send(f")
+    rolled = [randint(1, sides) for _ in range(num_of_rolls)]
+    total = sum(rolled)
+    await ctx.send(f"Rolling {num_of_rolls}d{sides}...\nYou rolled: {str(rolled)[1:-1]}\nTotal: {total}")
+
+
+bot.run("NjMzNDYzNzYzNTcxMzEwNjAz.XaUVKA.X18n4S_lIDET3TGdoX86KzPxQO8")
